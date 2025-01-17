2e4f28e21e6a73af1791b66e7ac24e4dec44f805
kushana <rashmisenadheera98@gmail.com>
2025-01-17 17:17:41 +0530

    Fixes Issues with frontend

--- lms_frontend/src/App.tsx
+++ lms_frontend/src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
+//import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 //import BookTable from './pages/bookTable/BookTable';
 import EditBook from './pages/editBook/EditBook';
 import Home from "./pages/Home/home/Home";
+import {Hero} from './pages/heroSection/Hero';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Hero />} />
+        <Route path="/home" element={<Home/>}/>
         <Route path="/edit-book/:id" element={<EditBook />} />
       </Routes>
     </Router>
   );
 };
-
 export default App;
-
-
-
-
-//Remove-Item -Recurse -Force node_modules
-//npm install
--- lms_frontend/src/components/navBar.tsx
+++ lms_frontend/src/components/navBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import lmslogo from '../assets/lmslogo1.png';
 import './navBar.css';
+import PersonIcon from '@mui/icons-material/Person';
 
 type NavBarProps = {};
 
@@ -8,7 +9,7 @@ const NavBar: React.FC<NavBarProps> = () => {
   return (
     <div className='navB'>
       <nav className='ord1'>
-        <div className='logoN'>LMS</div>
+        <div className='logoN'><img src={lmslogo} style={{height:'50px'}}/></div>
         <ul>
           <li></li>  
           <li>Books</li>
